fed6f561c848a702c3acf7d7c01433730d6c4e07
akshaydr <akshay.dr.11@gmail.com>
2017-11-08 20:24:34 +0530

    Working object detector with service call

--- scripts/object_detector.py
+++ scripts/object_detector.py
@@ -9,11 +9,37 @@ import rospy
 from std_srvs.srv import *
 from sensor_msgs.msg import Image
 
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+darknet_path = dir_path + '/../pyyolo/darknet'
+datacfg = dir_path + '/../pyyolo/darknet/cfg/coco.data'
+cfgfile = dir_path + '/../pyyolo/darknet/cfg/tiny-yolo.cfg'
+weightfile = dir_path + '/../pyyolo/tiny-yolo.weights'
+
 image = None
+
+def detect():
+    global image
+
+    img = image.transpose(2,0,1)
+    c, h, w = img.shape[0], img.shape[1], img.shape[2]
+
+    data = img.ravel()/255.0
+    data = np.ascontiguousarray(data, dtype=np.float32)
+    outputs = pyyolo.detect(w, h, c, data, 0.24, 0.5) #thresh, heir_thresh
+
+    return outputs
 
 def handle_request(req):
     print ("Recieved",req)
-    return SetBoolResponse(True, 'success')
+    r = detect()
+    print(r)
+    if(len(r) == 0):
+        return SetBoolResponse(False, str(r))
+    else:
+        return SetBoolResponse(True, str(r))
 
 def image_handler(img_msg):
     global image
@@ -27,5 +53,11 @@ if __name__ == "__main__":
     rospy.init_node('server')
     s = rospy.Service('my_service', SetBool, handle_request)
     rospy.Subscriber('/camera/image_raw', Image, image_handler)
+
+    pyyolo.init(darknet_path, datacfg, cfgfile, weightfile)
+
     print "Ready to recieve."
     rospy.spin()
+
+    pyyolo.cleanup()
+    print("Done Cleaning...")
